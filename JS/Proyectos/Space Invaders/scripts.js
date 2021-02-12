70c102a1574c786cfede597c51050e64a934c28a
BLANCO CAMPANDEGI KEVIN <44571028H@CEBANCDEA.EDU>
2021-02-12 17:59:41 +0100

    Space Invaders acabado y comentado

--- JS/Proyectos/Space Invaders/scripts.js
+++ JS/Proyectos/Space Invaders/scripts.js
@@ -1,27 +1,37 @@
+// Guardamos el canvas y su contexto en variables
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+// Cargamos las imágenes en variable
 var img_screen = document.getElementById("img_fondo");
 var img_brick = document.getElementById("img_brick");
 var img_ball = document.getElementById("img_ball");
 
+// Definimos un ancho y alto fijo al canvas
 canvas.width = 1500;
 canvas.height = 700;
 
+// Definimos un punto de inicio en la parte central inferior del lienzo para la bola
 var x = canvas.width / 2;
 var y = canvas.height - 30;
 
+// Definimos dos variables que se utilizarán para dibujar la bola en puntos distintos para que parezca que se mueve
 var dx = 2;
 var dy = -2;
 
+// Definimos el radio (o tamaño) de la bola
 var ballRadius = 10;
 
+// Definimos el tamaño de la pala con la que golpearemos la bola
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 
+// Definimos 2 variables booleanos para detectar si el usuario presiona las flechas derecha e izquierda
 var rightPressed = false;
 var leftPressed = false;
 
+// Definimos los atributos de los ladrillos
 var brickRowCount = 6;
 var brickColumnCount = 17;
 var brickWidth = 75;
@@ -30,6 +40,7 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
+// Creamos un array de ladrillos para cargarlos más tarde
 var bricks = [];
 for (c = 0; c < brickColumnCount; c++) {
 	bricks[c] = [];
@@ -42,19 +53,21 @@ for (c = 0; c < brickColumnCount; c++) {
 	}
 }
 
+// Definimos la puntuación y las vidas del jugador
 var score = 0;
 var lives = 3;
 
+// Función para dibujar en el canvas
 function draw() {
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpiamos el canvas para que el nuevo dibujo no esté solapada con el antiguo
 	// ctx.drawImage(img_screen, 0, 0, canvas.width, canvas.height); //Imagen estática de fondo
-	drawBricks();
-	drawBall();
-	drawPaddle();
-	drawScore();
-	drawLives();
-	collisionDetection();
+	drawBricks(); // Función para dibujar los ladrillos
+	drawBall(); // Función para dibujar la bola
+	drawPaddle(); // Función para dibujar la pala
+	drawScore(); // Función para dibujar la puntuación
+	drawLives(); // Función para dibujar las vidas
+	collisionDetection(); // Función para detectar la colisión entre la bola y los ladrillos y cambiar la puntuación y mostrar el modal
 	x += dx;
 	y += dy;
 
@@ -92,6 +105,7 @@ function draw() {
 
 }
 
+// Función para dibujar los ladrillos
 function drawBricks() {
 	for (c = 0; c < brickColumnCount; c++) {
 		for (r = 0; r < brickRowCount; r++) {
@@ -110,6 +124,7 @@ function drawBricks() {
 	}
 }
 
+// Función para dibujar la bola
 function drawBall() {
 
 	ctx.beginPath();
@@ -120,6 +135,7 @@ function drawBall() {
 
 }
 
+// Función para dibujar la pala
 function drawPaddle() {
 	ctx.beginPath();
 	ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
@@ -128,26 +144,30 @@ function drawPaddle() {
 	ctx.closePath();
 }
 
+// Escuchadores de eventos para las flechas y el ratón
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
+// Función para detectar cuando se pulsa la tecla de la flecha derecha/izquierda
 function keyDownHandler(e) {
-	if (e.keyCode == 39) {
+	if (e.keyCode == 39) { //Flecha Derecha
 		rightPressed = true;
-	} else if (e.keyCode == 37) {
+	} else if (e.keyCode == 37) { //Flecha Izquierda
 		leftPressed = true;
 	}
 }
 
+// Función para detectar cuando se deja de pulsa una tecla
 function keyUpHandler(e) {
-	if (e.keyCode == 39) {
+	if (e.keyCode == 39) { //Flecha derecha
 		rightPressed = false;
-	} else if (e.keyCode == 37) {
+	} else if (e.keyCode == 37) { //Flecha Izquierda
 		leftPressed = false;
 	}
 }
 
+// Función para detectar el movimiento del ratón, y guardar la posición de ésta para dibujar la pala en dicho sitio
 function mouseMoveHandler(e) {
 	var relativeX = e.clientX - canvas.offsetLeft;
 	if (relativeX > 0 && relativeX < canvas.width) {
@@ -155,6 +175,7 @@ function mouseMoveHandler(e) {
 	}
 }
 
+// Función para detectar la colisión entre la bola y los ladrillos y cambiar la puntuación y mostrar el modal si es necesario
 function collisionDetection() {
 	for (c = 0; c < brickColumnCount; c++) {
 		for (r = 0; r < brickRowCount; r++) {
@@ -174,19 +195,21 @@ function collisionDetection() {
 	}
 }
 
+// Función para dibujar la puntuación
 function drawScore() {
 	ctx.font = "16px Arial";
 	ctx.fillStyle = "#0095DD";
 	ctx.fillText("Puntuación: " + score, 8, 20);
 }
 
+// Función para dibujar las vidas
 function drawLives() {
 	ctx.font = "16px Arial";
 	ctx.fillStyle = "#0095DD";
 	ctx.fillText("Vidas: " + lives, canvas.width - 65, 20);
 }
 
-var intervalo = setInterval(draw, 5);
+var intervalo = setInterval(draw, 10); // Intervalo para dibujar el canvas cada 10 milisegundos
 // draw();
 
 // ----- MODAL  -----
@@ -201,6 +224,7 @@ salir.addEventListener("click", jugarSalir, false);
 salir.addEventListener("mouseover", cambiarTexto, false);
 salir.addEventListener("mouseout", cambiarTexto, false);
 
+// Función para detener el intervalo para dibujar el canvas y mostrar un mensaje según el resultado del juego
 function mensajeModal() {
 
 	clearInterval(intervalo);
@@ -213,6 +237,7 @@ function mensajeModal() {
 
 }
 
+// Función para recargar la página o mostrar un mensaje de salida y cerrar la ventana, según lo que elija el usuario
 function jugarSalir(e) {
 
 	if (e.target.id == "seguir") {
@@ -226,6 +251,7 @@ function jugarSalir(e) {
 
 }
 
+// Función para cambiar el texto del modal cuando el usuario pone el ratón encima de uno de los botones del modal
 function cambiarTexto(e) {
 
 	if (e.target.id == "seguir" && e.type == "mouseover") {
@@ -248,6 +274,7 @@ function cambiarTexto(e) {
 
 }
 
+// Función para cerrar el modal cuando le da a la X en la posición superior derecha
 span.onclick = function () {
 
 	document.getElementsByTagName("body")[0].innerHTML = `<h2>Saliendo del juego, tenga un buen día</h2>`;
